20e47e2507ff5304a168247e97bb822e783501cb
Ishan Patwa <Ishan@Ishans-MacBook-Pro.local>
2015-10-03 18:55:27 -0400

    final version

--- GossipEmulator.scala
+++ GossipEmulator.scala
@@ -10,6 +10,9 @@ object Constants{
           val CounterLimit = 10; //stopping condition for gossip protocols
 }
 
+/**
+ * workers used for Line and Full mesh topology in both algorithms.
+ */
 class Workers(arrActors: Array[ActorRef], numNodes: Int) extends Actor {
 
   //array keeping track of no. of messages each node has recieved.
@@ -32,7 +35,10 @@ class Workers(arrActors: Array[ActorRef], numNodes: Int) extends Actor {
     case "Rumour_push-sum" =>
       println("Starting the push_Sum algorithm");
       transmit_pushSumMessage;
+    case "Full_Mesh_Rumour_push-sum" =>
+       transmitGossipInMesh
     case (s_m : BigDecimal,w_m : BigDecimal) => //println("recieved a tuple (s,w) :" + s_m.toString + "  " + w_m.toString);
+      println("current ratio : " + ratio);
       updateCounters(s_m,w_m);
       transmit_pushSumMessage;
     case "Full_Mesh_Rumour_gossip" =>
@@ -87,10 +93,10 @@ class Workers(arrActors: Array[ActorRef], numNodes: Int) extends Actor {
     }
   }
   
-  //message dissemination in line tpology - PUSH-SUM algorithm
+  //message dissemination in line topology - PUSH-SUM algorithm
   def transmit_pushSumMessage = {
             s = s/2 ; w = w/2;
-            var message = (s/2,w/2);
+            var message = (s,w);
             val r = scala.util.Random;
             var actorNumber = self.path.name.toInt;  //ith actor in the array.
             var actNum = r.nextInt(numNodes);
@@ -111,7 +117,7 @@ class Workers(arrActors: Array[ActorRef], numNodes: Int) extends Actor {
   
   def tranmit_pushSumFullMesh = {  
             s = s/2 ; w = w/2;
-            var message = (s/2,w/2);
+            var message = (s,w);
             val r = scala.util.Random;
             var actorNumber = self.path.name.toInt;  //ith actor in the array.
             var actNum = r.nextInt(numNodes);
@@ -125,17 +131,21 @@ class Workers(arrActors: Array[ActorRef], numNodes: Int) extends Actor {
   
   def updateCounters(s_m :BigDecimal , w_m : BigDecimal) = {
             var new_ratio =  (s + s_m)/(w + w_m);
-            if(new_ratio - ratio > convergenceCounter || new_ratio -ratio < (-1)*convergenceCounter) {
+            if(new_ratio - ratio > Constants.ConvergenceError || new_ratio -ratio < (-1)*Constants.ConvergenceError) {
                   convergenceCounter = 0;
             }else {
                 convergenceCounter += 1;
             }
+            ratio = new_ratio;
             println("convergenceCounter for  : " + self.path.name + " " + convergenceCounter.toString());
            if(convergenceCounter == 3 ) context.stop(self);       
   }
 
 }
 
+/**
+ * workers used to implement a 3D topology and Imperfect 3D topology.
+ */
 class Workers3D(arrActors: Array[Array[Array[ActorRef]]], numNodes: Int, actorNum : Int) extends Actor {
 
   var counter = Array.ofDim[Int](numNodes, numNodes, numNodes);
@@ -158,7 +168,13 @@ class Workers3D(arrActors: Array[Array[Array[ActorRef]]], numNodes: Int, actorNu
       transmit3DGossip;
     case "3D_rumour_push-sum" => println("starting push sum algorithm for 3D grid")
     transmit3DpushSumMessage;
-    case (s_m : BigDecimal , w_m : BigDecimal ) => updateCounters(s_m,w_m)
+    case "Imp3D_rumour_gossip" =>
+      println(self.path.name + "receievd a message");
+      transmitImp3DGossip;
+    case "Imp3D_rumour_push-sum" => println("starting push sum algorithm for Imp 3D grid")
+    transmitImp3dPushSum;
+    case (s_m : BigDecimal , w_m : BigDecimal ) => println("recieved a tuple (s,w) :" + s_m.toString + "  " + w_m.toString);
+    updateCounters(s_m, w_m)
     transmit3DpushSumMessage;
 
   }
@@ -274,13 +290,170 @@ class Workers3D(arrActors: Array[Array[Array[ActorRef]]], numNodes: Int, actorNu
     }
   }
   
-def updateCounters(s_m :BigDecimal , w_m : BigDecimal) = {
+  def transmitImp3dPushSum = {
+     var index = self.path.name.split(",");
+    val r = scala.util.Random;
+    var randomGen = r.nextInt(6);
+    val rangeStart = 0; val rangeEnd = numNodes - 1;
+    var isSent: Boolean = false;
+    s = s/2;
+    w = w/2;
+    var message = (s,w);
+    while (!isSent) {
+
+      randomGen = r.nextInt(7);
+
+      if (randomGen == 0) {
+        if (index(0).toInt - 1 >= 0) {
+          arrActors(index(0).toInt - 1)(index(1).toInt)(index(2).toInt) ! message;
+          isSent = true;
+        }
+
+      } else if (randomGen == 1) {
+        if (index(1).toInt - 1 >= 0) {
+          arrActors(index(0).toInt)(index(1).toInt - 1)(index(2).toInt) ! message;
+          isSent = true;
+
+        }
+
+      } else if (randomGen == 2) {
+        if (index(2).toInt - 1 >= 0) {
+          arrActors(index(0).toInt)(index(1).toInt)(index(2).toInt - 1) ! message;
+          isSent = true;
+        }
+
+      } else if (randomGen == 3) {
+        if (index(0).toInt + 1 < numNodes) {
+          arrActors(index(0).toInt + 1)(index(1).toInt)(index(2).toInt) ! message;
+          isSent = true;
+        }
+
+      } else if (randomGen == 4) {
+
+        if (index(1).toInt + 1 < numNodes) {
+          arrActors(index(0).toInt)(index(1).toInt + 1)(index(2).toInt) ! message;
+          isSent = true;
+        }
+
+      } else if (randomGen == 5) {
+        if (index(2).toInt + 1 < numNodes) {
+          arrActors(index(0).toInt)(index(1).toInt)(index(2).toInt + 1) ! message;
+          isSent = true;
+        }
+      }else if(randomGen == 7) {
+            var isNotNeighbor : Boolean = false;
+            var i = -1;
+            var j = -1;
+            var k = -1;
+             while(isNotNeighbor){
+                        i = r.nextInt(numNodes-1);
+                        j = r.nextInt(numNodes-1);
+                        k = r.nextInt(numNodes-1);
+                       isNotNeighbor = checkAgainsNeighbors(i,j,k, index) & inLimits(i,j,k);
+             }
+             arrActors(i)(j)(k) ! message;
+             isSent = true;
+             
+      }
+    }
+      
+  }
+  
+  def transmitImp3DGossip = {
+    var index = self.path.name.split(",");
+    counter(index(0).toInt)(index(1).toInt)(index(2).toInt) += 1;
+    if (counter(index(0).toInt)(index(1).toInt)(index(2).toInt) == Constants.CounterLimit) {
+      println("Worker" + index.toString + "is stopping");
+      context.stop(self);
+    }
+    val r = scala.util.Random;
+    var randomGen = r.nextInt(6);
+    val rangeStart = 0; val rangeEnd = numNodes - 1;
+    var isSent: Boolean = false;
+    while (!isSent) {
+
+      randomGen = r.nextInt(7);
+
+      if (randomGen == 0) {
+        if (index(0).toInt - 1 >= 0) {
+          arrActors(index(0).toInt - 1)(index(1).toInt)(index(2).toInt) ! "Imp3D_rumour_gossip";
+          isSent = true;
+        }
+
+      } else if (randomGen == 1) {
+        if (index(1).toInt - 1 >= 0) {
+          arrActors(index(0).toInt)(index(1).toInt - 1)(index(2).toInt) ! "Imp3D_rumour_gossip";
+          isSent = true;
+
+        }
+
+      } else if (randomGen == 2) {
+        if (index(2).toInt - 1 >= 0) {
+          arrActors(index(0).toInt)(index(1).toInt)(index(2).toInt - 1) ! "Imp3D_rumour_gossip";
+          isSent = true;
+        }
+
+      } else if (randomGen == 3) {
+        if (index(0).toInt + 1 < numNodes) {
+          arrActors(index(0).toInt + 1)(index(1).toInt)(index(2).toInt) ! "Imp3D_rumour_gossip";
+          isSent = true;
+        }
+
+      } else if (randomGen == 4) {
+
+        if (index(1).toInt + 1 < numNodes) {
+          arrActors(index(0).toInt)(index(1).toInt + 1)(index(2).toInt) ! "Imp3D_rumour_gossip";
+          isSent = true;
+        }
+
+      } else if (randomGen == 5) {
+        if (index(2).toInt + 1 < numNodes) {
+          arrActors(index(0).toInt)(index(1).toInt)(index(2).toInt + 1) ! "Imp3D_rumour_gossip";
+          isSent = true;
+        }
+      } else if(randomGen == 7) {
+            var isNotNeighbor : Boolean = false;
+            var i = -1;
+            var j = -1;
+            var k = -1;
+             while(isNotNeighbor){
+                        i = r.nextInt(numNodes-1);
+                        j = r.nextInt(numNodes-1);
+                        k = r.nextInt(numNodes-1);
+                       isNotNeighbor = checkAgainsNeighbors(i,j,k, index) & inLimits(i,j,k);
+             }
+             arrActors(i)(j)(k) ! "Imp3D_rumour_gossip";
+             isSent = true;
+             
+      }
+    }
+  }
+    def checkAgainsNeighbors(i: Int, j : Int , k : Int , index : Array[String]) : Boolean = {
+            //here we will check if i , j , k == any neighbour of index.
+            var coordinates = (i,j,k);
+            var n0 = (index(0).toInt,index(1).toInt,index(2).toInt);
+            var n1 = (index(0).toInt - 1,index(1).toInt,index(2).toInt);
+            var n2 = (index(0).toInt,index(1).toInt-1,index(2).toInt);
+            var n3 = (index(0).toInt,index(1).toInt,index(2).toInt-1);
+            var n4 = (index(0).toInt  + 1,index(1).toInt,index(2).toInt);
+            var n5 = (index(0).toInt,index(1).toInt+1,index(2).toInt);
+            var n6 = (index(0).toInt,index(1).toInt,index(2).toInt+1);
+            if( coordinates == n1 || coordinates == n2 || coordinates == n3 || coordinates == n4 || coordinates == n5 || coordinates == n6 || coordinates == n0) return true;
+            else return false;
+  }
+  def inLimits(i : Int, j : Int, k :Int ) : Boolean = {
+          if((i >=0  && i < numNodes) && (j >=0  && j < numNodes) && (k >=0  && k < numNodes)  ) return true;
+          else return false;
+  }
+  
+  def updateCounters(s_m :BigDecimal , w_m : BigDecimal) = {
             var new_ratio =  (s + s_m)/(w + w_m);
-            if(new_ratio - ratio > convergenceCounter || new_ratio -ratio < (-1)*convergenceCounter) {
+            if(new_ratio - ratio > Constants.ConvergenceError || new_ratio -ratio < (-1)*Constants.ConvergenceError) {
                   convergenceCounter = 0;
             }else {
                 convergenceCounter += 1;
             }
+            ratio = new_ratio;
             println("convergenceCounter for  : " + self.path.name + " " + convergenceCounter.toString());
            if(convergenceCounter == 3 ) context.stop(self);       
   }
@@ -310,7 +483,7 @@ object Master {
       case "line" => createLineTopology(numNodes, system,algorithm);
       case "Full" => createFullMeshTopology(numNodes, system,algorithm);
       case "3D" => create3DgridTopology(numNodes, system,algorithm);
-      case "imp#D" => createImperfect3DgridTopology(numNodes,system,algorithm);
+      case "imp3D" => createImperfect3DgridTopology(numNodes,system,algorithm);
     }
 
   }
@@ -340,17 +513,18 @@ object Master {
     * create a imperfect 3D grid topology and start a rumour
     */
   def createImperfect3DgridTopology(numNodes :Int , system : ActorSystem,algorithm : String){
-          var arrActors: Array[ActorRef] = new Array[ActorRef](numNodes);
-          //the imperfect 3D grid has 5 fixed neighbors and a one random neighbor.
-          //the question is to fix which 5 ? and leave the other? and what if there are two links to one node?
-          //for now we are avoiding multiple links to a node. and fixing [i-1][j][k] .... [i][j-1][k] 
+          var arrActors = Array.ofDim[ActorRef](numNodes, numNodes, numNodes);
+          var counter = 0;
           for (i <- 1 to numNodes) {
             for (j <- 1 to numNodes) {
               for (k <- 1 to numNodes) {
-                                  //figure out the topology structure.
+                               counter += 1;
+                               arrActors(i - 1)(j - 1)(k - 1) = system.actorOf(Props(new Workers3D(arrActors, numNodes,counter)), name = (i - 1).toString + "," + (j - 1).toString + "," + (k - 1).toString);
               }
             }
           }
+        println("sending first message in Imperfect  3D topology");
+        arrActors(0)(0)(0) ! "Imp3D_rumour_" + algorithm; 
   }
   
   /*
@@ -367,7 +541,6 @@ object Master {
 
   }
 
-  
   /*
    * create a full mesh topology and start sending a rumour for the created topology
    */
